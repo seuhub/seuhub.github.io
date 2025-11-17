68751eb050e6f3c39d0d197b707e8db4f9b844fe
seuhub <seuzerhub@outlook.com>
2025-11-17 13:09:27 +0800

    hugo
    hugo

---
+++ myblog/content/post/first-post.md
@@ -0,0 +1,8 @@
++++
+date = '2025-11-17T10:57:32+08:00'
+draft = true
+title = 'First Post'
++++
+### 你好
+
+#### hello blog
---
+++ test.py
@@ -0,0 +1,15 @@
+# https://hugo.opendocs.io/getting-started/usage/
+
+# 添加环境变量……   新建
+
+# hugo version 验证是否成功安装
+
+# cd E:\pythoncode\seuhub.github.io  进入当前目录
+
+# hugo new site myblog   创建博客
+
+# hugo new theme Lovelt  创建主题
+
+
+
+
